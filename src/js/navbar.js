f5689e19cff400b94f8a4fa352c38a55c64acf4b
Vresod <lukebinkofsky@gmail.com>
2024-09-12 17:02:59 -0400

    use a THIRD, DIFFERENT SOLUTION for the navbar modularization

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,12 +1,3 @@
-import fs from "fs";
-import path from "path";
-import navbar_code from 'bundle-text:../navbar.html'
-
-let insert_navbar = () => {
-	let navbar_div = document.querySelector("div#insert_navbar");
-	navbar_div.outerHTML = navbar_code;
-}
-
 let select_active_page = () => {
 	/*
 	* Grabs the navbar link that corresponds to the currently active page and activates it
@@ -21,6 +12,5 @@ let select_active_page = () => {
 };
 
 (() => { // this is the normal way of doing it but it feels like I'm writing lisp
-	insert_navbar();
 	select_active_page();
 })();
