c6d6a42f3d4f92840c4bc5b0f1b3845572630e87
Vresod <vresod@proton.me>
2024-09-16 14:31:46 -0400

    fuck with the footer more

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,8 @@
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap';
+
+// Only runs in development and will be stripped in production builds.
+if (process.env.NODE_ENV !== 'production') {
+	let footer = document.querySelector(".footer-statement");
+	footer.innerHTML += "<p class='fs-1'>DEVELOPMENT BUILD</p>";
+}
