60458759293d9be8d1d3dc116b69dab304622e85
Vresod <vresod@proton.me>
2024-10-15 14:55:15 -0400

    update about me page, add a little flair, blah blah blah

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -2,13 +2,22 @@ let select_active_page = () => {
 	/*
 	* Grabs the navbar link that corresponds to the currently active page and activates it
 	*/
-	let navlinks = document.querySelectorAll("a.nav-link");
-	navlinks.forEach(link => {
-		if (link.href === document.location.href) {
-			link.classList.add("active");
-			link.ariaCurrent = "page";
+	let navlinks = document.querySelectorAll("a.nav-link, a.dropdown-item");
+	for (link of navlinks) {
+		if (link.href != document.location.href) {
+			continue;
 		}
-	});
+		link.classList.add("active");
+		link.ariaCurrent = "page";
+		// walk the node tree until we get to the parent element, so we can mark it as active as well
+		if (link.classList.contains("dropdown-item")) {
+			let navParent = link.parentElement;
+			while (!navParent.classList.contains("nav-item")) {
+				navParent = navParent.parentElement;
+			}
+			navParent.querySelector("a.nav-link").classList.add("active");
+		}
+	}
 };
 
 (() => { // this is the normal way of doing it but it feels like I'm writing lisp
