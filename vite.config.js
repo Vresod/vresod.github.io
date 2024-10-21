e1e69f1ea688c5f845f8144a9f3863db61ee3cb3
Vresod <vresod@proton.me>
2024-10-21 14:40:23 -0400

    make builds work better I think

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,28 @@
 import { resolve } from 'path'
 import posthtml from '@vituum/vite-plugin-posthtml'
+import { defineConfig } from 'vite'
 
-export default {
+export default defineConfig({
 	root: resolve(__dirname, 'src'),
+	appType: 'mpa',
 	build: {
-		outDir: '../dist'
+		outDir: '../dist',
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "src/index.html"),
+				projects: resolve(__dirname, "src/projects.html"),
+				contact: resolve(__dirname, "src/contact.html"),
+				aboutme: resolve(__dirname, "src/aboutme.html"),
+				"aboutme/cars": resolve(__dirname, "src/aboutme/cars.html"),
+				"projects/aue-py": resolve(__dirname, "src/projects/aue-py.html"),
+				"projects/not-ddg-bangs": resolve(__dirname, "src/projects/not-ddg-bangs.html"),
+			}
+		}
 	},
 	server: {
 		port: 8080
 	},
 	plugins: [
 		posthtml()
-	]
-}
+	],
+})
